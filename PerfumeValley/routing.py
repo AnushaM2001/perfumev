ba7dcf202001ea4abe8226e02b3f682d91c43bdd
Anusha Maada <anushanaidu.m123@gmail.com>
2025-08-11 17:51:18 +0530

    changes

---
+++ PerfumeValley/routing.py
@@ -0,0 +1,10 @@
+# asgi.py or routing.py
+from django.urls import re_path
+
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/cart/(?P<user_id>\w+)/$', consumers.CartConsumer.as_asgi()),
+    re_path(r'ws/notifications/$', consumers.NotificationConsumer.as_asgi()),
+
+]
