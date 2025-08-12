745d7aff7caad37557beae76a01a512383c9033d
AnushaM2001 <144044462+AnushaM2001@users.noreply.github.com>
2025-08-12 18:52:10 +0530

    Update asgi.py

--- PerfumeValley/asgi.py
+++ PerfumeValley/asgi.py
@@ -11,17 +11,26 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from whitenoise import WhiteNoise
 import django
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PerfumeValley.settings')
 django.setup()
 
 from . import routing
 
+django_asgi_app = get_asgi_application()
+
+# Wrap with WhiteNoise
+static_root_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../staticfiles')
+django_asgi_app = WhiteNoise(django_asgi_app, root=static_root_path) 
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
         )
     ),
 })
+
